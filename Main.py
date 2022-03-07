28a6b19ba58497d781638abbf4f65769de8ec6b5
Marc M <marc.morcos1@hotmail.com>
2022-03-07 11:04:40 -0500

    fix kill key

--- Main.py
+++ Main.py
@@ -70,7 +70,7 @@ class Main:
                 if event.key == pygame.K_COMMA:
                     printMaze(self.maze.maze_array)
                     print("pac_man_x: {} pac_man_y: {}".format(self.player.x, self.player.y))
-                if event.key == pygame.K_EQUALS:
+                if event.key == pygame.K_EQUALS and neatMode:
                     player.lives = -1
                     self.game_state = "lose"
             if event.type == pygame.QUIT:
