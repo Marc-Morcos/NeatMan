f0374cf1fe78c640b24dc2773371110bed839711
Marc M <marc.morcos1@hotmail.com>
2022-03-06 23:41:22 -0500

    changed how 1 output works

--- movementAlgos.py
+++ movementAlgos.py
@@ -382,11 +382,7 @@ def rotatingCameraNeatHelper(pacman, maze, ghosts, pellets, power_pellets, fruit
             if((not wrapAroundY) and (cameraMin[1]+y<0 or cameraMin[1]+y>=MapSizeY)): continue
             smallCamera[y, x] = fullGrid[(cameraMin[0]+x)%MapSizeX,(cameraMin[1]+y)%MapSizeY]
     
-    #rotation based on pacman
-    if(oneOutput):
-                rotateDir = ghost.look_dir
-    else:
-                rotateDir = ghost.move_dir
+    rotateDir = ghost.move_dir
     if(rotateCamera):
         if rotateDir == RIGHT:
             smallCamera = np.rot90(smallCamera, 1)
